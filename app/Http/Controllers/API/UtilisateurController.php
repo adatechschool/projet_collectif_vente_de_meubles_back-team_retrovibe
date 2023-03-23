47156a0e4a239bbef8ca2b86d26cff794ab3baff
HelderF2821 <helder.ferreira2821@gmail.com>
2023-03-23 11:44:50 +0100

    mdp crypter

--- app/Http/Controllers/API/UtilisateurController.php
+++ app/Http/Controllers/API/UtilisateurController.php
@@ -1,13 +1,10 @@
 <?php
 /* MA PAGE CONTROLLER POUR MA TABLE UTILISATEUR.*/
-
-
 namespace App\Http\Controllers\API;
-
 use App\Http\Controllers\Controller;
 use App\Models\Utilisateur;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Hash;
 class UtilisateurController extends Controller
 {
     /********** GET (globaux) **********/
@@ -15,46 +12,46 @@ class UtilisateurController extends Controller
     {
         // On récupère tous les utilisateurs
         $utilisateur = Utilisateur::all();
-
         // On retourne les informations des utilisateurs en JSON
         return response()->json($utilisateur)->header('Access-Control-Allow-Origin', '*');
     }
-
     /********** POST **********/
     public function store(Request $request)
     {
-        // pour la création, on exige que ces trois infos soient obligatoirement remplies. (nom, email, mdp) 
+        // pour la création, on exige que ces trois infos soient obligatoirement remplies. (nom, email, mdp)
         $this->validate($request, [
             'nom' => 'required|max:100',    // 100 caractères maximum.
             'email' => 'required|email|unique:utilisateurs',       // le mail est unique pour chaque utilisateur.
             'mot_de_passe' => 'required|min:8'      // 8 caractères minimum pour le mdp.
         ]);
-
         // On crée un nouvel utilisateur.
         $utilisateur = Utilisateur::create([
             'nom' => $request->nom,
             'prenom' => $request->prenom,
             'email' => $request->email,
             // le mot de passe est crypté avant son insertion dans la BDD.
-            'mot_de_passe' => $request->mot_de_passe, // ajouter bcrypt() pour crypyer le mdp cf UserController
+            'mot_de_passe' => bcrypt($request->mot_de_passe),
             'telephone' => $request->telephone,
             'adresse' => $request->adresse,
             'code_postal' => $request->code_postal,
             'ville' => $request->ville
-
         ]);
-
         // On retourne les informations du nouvel utilisateur en JSON.
         return response()->json($utilisateur, 201)->header('Access-Control-Allow-Origin', '*');
     }
-
     /********** GET (by one) **********/
-    public function show(Utilisateur $utilisateur)
+    public function show(Utilisateur $utilisateur, Request $request)
     {
-        // On retourne les informations d'un utilisateur précis en JSON.
+        // On vérifie si le mot de passe entré correspond à celui stocké dans la base de données.
+        if ($request->email !== $utilisateur->email) {
+            return response()->json(['message' => 'Le mail est incorrect.'], 401);
+            // On vérifie si le mot de passe entré correspond à celui stocké dans la base de données.
+        } else if (!Hash::check($request->mot_de_passe, $utilisateur->mot_de_passe)) {
+            return response()->json(['message' => 'Le mot de passe est incorrect.'], 401);
+        }
+        // On retourne les informations de l'utilisateur en JSON.
         return response()->json($utilisateur)->header('Access-Control-Allow-Origin', '*');
     }
-
     /********** PUT **********/
     public function update(Request $request, Utilisateur $utilisateur)
     {
@@ -63,30 +60,26 @@ class UtilisateurController extends Controller
             'email' => 'email|unique:utilisateurs',
             'mot_de_passe' => 'min:8'
         ]);
-
         // On modifie les informations de l'utilisateur
         $utilisateur->update([
             'nom' => $request->nom ?? $utilisateur->nom,
             'prenom' => $request->prenom ?? $utilisateur->prenom,
             'email' => $request->email ?? $utilisateur->email,
-            'mot_de_passe' => $request->mot_de_passe,
+            'mot_de_passe' => bcrypt($request->mot_de_passe),
             'telephone' => $request->telephone ?? $utilisateur->telephone,
             'adresse' => $request->adresse ?? $utilisateur->adresse,
             'code_postal' => $request->code_postal ?? $utilisateur->code_postal,
             'ville' => $request->ville ?? $utilisateur->ville
         ]);
-
         // On retourne les informations du nouvel utilisateur en JSON.
         return response()->json($utilisateur)->header('Access-Control-Allow-Origin', '*');
     }
-
     /********** DELETE **********/
     public function destroy(Utilisateur $utilisateur)
     {
         // On supprime l'utilisateur.
         $utilisateur->delete();
-
         // On retourne la réponse JSON.
         return response()->json()->header('Access-Control-Allow-Origin', '*');
-    }    
+    }
 }
